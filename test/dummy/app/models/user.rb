b56d952cefe4a1c17ec9b570c896a57a876b42a0
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-02-05 18:26:11 +0900

    configure index mapping for test models

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include Elasticsearch::Model
+
   has_many :posts
+
+  index_name 'users'
+  document_type 'user'
+
+  mappings do
+    indexes :name, type: 'text', analyzer: 'english'
+  end
 end
