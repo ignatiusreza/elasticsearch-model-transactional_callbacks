3cd52e0344d08e9fe2f0d8d10673de26c7dbfd7c
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-02-05 14:30:25 +0900

    add some test models

---
+++ test/dummy/db/migrate/20190205050439_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.belongs_to :user
+      t.string :subject
+      t.timestamps
+    end
+  end
+end
